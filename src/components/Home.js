25737558c0398ca09eea42fab3d29043ed86b734
claudiaoj <claudiaortizjabre@gmail.com>
2023-06-27 16:15:50 -0400

    edita los post

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,65 +1,86 @@
 // import { async } from "regenerator-runtime";
 // import { async } from "regenerator-runtime";
 import {
-  crearPost, mostrarPost, borrarPost, addLike, getUser, getLikes, removeLike,
-} from '../lib/index.js';
+  crearPost,
+  mostrarPost,
+  borrarPost,
+  addLike,
+  getUser,
+  getLikes,
+  removeLike,
+  getPost,
+  updatePost,
+} from "../lib/index.js";
 
 export const Home = (onNavigate) => {
   // Variables de divs del Dom
-  const HomeDiv = document.createElement('div');
-  const logoDiv = document.createElement('div');
-  const logoImg = document.createElement('img');
-  const postSection = document.createElement('section');
-  const postDiv = document.createElement('div');
-  const postFeedDiv = document.createElement('div');
+  const HomeDiv = document.createElement("div");
+  const logoDiv = document.createElement("div");
+  const logoImg = document.createElement("img");
+  const postSection = document.createElement("section");
+  const postDiv = document.createElement("div");
+  const postFeedDiv = document.createElement("div");
   // Variable de botón salir
-  const buttonLogout = document.createElement('button');
-  const logoutImg = document.createElement('img');
+  const buttonLogout = document.createElement("button");
+  const logoutImg = document.createElement("img");
   // Variables de formulario
-  const titleWelcome = document.createElement('h2');
-  const postPublish = document.createElement('input');
-  const buttonPublish = document.createElement('button');
+  const titleWelcome = document.createElement("h2");
+  const postPublish = document.createElement("input");
+  const buttonPublish = document.createElement("button");
 
   // Atributos de variables DOM
-  HomeDiv.className = 'container_all';
-  logoDiv.className = 'div_logo';
-  postSection.className = 'section_post';
-  postDiv.className = 'container_post';
+  HomeDiv.className = "container_all";
+  HomeDiv.id = "id-container-all";
+  logoDiv.className = "div_logo";
+  postSection.className = "section_post";
+  postDiv.className = "container_post";
+  postDiv.id = "id-container-post";
   // Atributo botón salir
-  logoutImg.className = 'img_logout';
-  logoutImg.src = 'images/logout1.png';
-  buttonLogout.className = 'logout_button';
+  logoutImg.className = "img_logout";
+  logoutImg.src = "images/logout1.png";
+  buttonLogout.className = "logout_button";
 
   // Atributos de imagen del título
-  logoImg.className = 'img_logo';
-  logoImg.src = 'images/MonuTrip1.png';
+  logoImg.className = "img_logo";
+  logoImg.src = "images/MonuTrip1.png";
   // Atributos de formulario
-  titleWelcome.className = 'welcome_title';
-  titleWelcome.textContent = 'Bienvenida(o)';
-  postPublish.id = 'post-input';
-  postPublish.className = 'post_input';
-  postPublish.placeholder = ' Comparte tu experiencia....';
-  postPublish.rows = '4'; // para que sean 4 lineas
-  buttonPublish.className = 'publish_button';
-  buttonPublish.textContent = 'Publicar';
+  titleWelcome.className = "welcome_title";
+  titleWelcome.textContent = "Bienvenida(o)";
+  postPublish.id = "post-input";
+  postPublish.className = "post_input";
+  postPublish.placeholder = " Comparte tu experiencia....";
+  //postPublish.rows = "4"; // para que sean 4 lineas
+  buttonPublish.className = "publish_button";
+  buttonPublish.textContent = "Publicar";
   // Atributos de publicaciones creadas
-  postFeedDiv.id = 'post-feed';
+  postFeedDiv.id = "post-feed";
 
   // Event Listener de botones
-  buttonLogout.addEventListener('click', () => onNavigate('/'));
 
-  buttonPublish.addEventListener('click', (e) => {
+  //Botón salir
+  buttonLogout.addEventListener("click", () => onNavigate("/"));
+
+  //Botón publicar
+  buttonPublish.addEventListener("click", async (e) => {
     e.preventDefault();
-    const contentInput = document.getElementById('post-input').value;
-    crearPost(contentInput).then(() => {
-      // alert("Hola");
-    });
+    if (editMode === false) {
+      const contentInput = document.getElementById("post-input").value;
+      crearPost(contentInput).then(() => {});
+    } else {
+      const postInput = document.querySelector("#post-input");
+      const updatedContent = postInput.value;
+      await updatePost(postIdEditing, { contenido: updatedContent });
+      mostrarPost();
+      editMode = false;
+      postIdEditing = "";
+    }
   });
+  let postIdEditing = "";
+  let editMode = false;
 
-  const contentFeed = document.getElementById('post-feed');
-
+  //Muestra los post, botonos editar, eliminar y likes en pantalla
   mostrarPost((querySnapshot) => {
-    let html = '';
+    let html = "";
     querySnapshot.forEach((doc) => {
       const publicacion = doc.data();
       html += `
@@ -78,37 +99,51 @@ export const Home = (onNavigate) => {
       </div>
       `;
 
-      limpiarInput(); // limpia input
+      limpiarInput();
     });
 
     postFeedDiv.innerHTML = html;
     likeEvent();
-    // limpia input
 
+    //Limpia input
     function limpiarInput() {
-      document.getElementById('post-input').value = '';
+      document.getElementById("post-input").value = "";
     }
 
-    const buttonDelete = postFeedDiv.querySelectorAll('.button_delete');
+    //Boton borrar
+    const buttonDelete = postFeedDiv.querySelectorAll(".button_delete");
     buttonDelete.forEach((btn) => {
-      btn.addEventListener('click', ({ target: { dataset } }) => {
+      btn.addEventListener("click", ({ target: { dataset } }) => {
         borrarPost(dataset.id);
       });
     });
 
-    /* const buttonEdit = postFeedDiv.querySelectorAll(".button_edit");
+    //Editar post
+    const postForm = document.getElementById("id-container-post");
+    const buttonEdit = postFeedDiv.querySelectorAll(".button_edit");
+
     buttonEdit.forEach((btn) => {
-      btn.addEventListener("click", (e) => {
-        console.log(e.target.dataset.id);
+      btn.addEventListener("click", async (e) => {
+        //console.log(editMode);
+        editMode = true;
+        //console.log(editMode);
+        const postId = e.target.dataset.id; //obtiene el ID de un post específico cuando se hace clic en un botón de editar
+        postIdEditing = postId;
+        const doc = await getPost(postId);
+        const task = doc.data(); //accede a los datos del post
+        const postInput = postForm.querySelector("#post-input");
+        if (postInput) {
+          postInput.value = task.contenido;
+        }
       });
-    }); */
+    });
   });
 
-  // Evento para dar likes
+  // Boton de likes
   function likeEvent() {
-    const likeButton = postFeedDiv.querySelectorAll('.like_btn'); // tomamos el valor del selector
+    const likeButton = postFeedDiv.querySelectorAll(".like_btn"); // tomamos el valor del selector
     likeButton.forEach((button) => {
-      button.addEventListener('click', () => {
+      button.addEventListener("click", () => {
         const likeValue = button.id;
         const userId = getUser().uid;
         getLikes(likeValue).then((postLike) => {
--- src/lib/index.js
+++ src/lib/index.js
@@ -3,8 +3,7 @@ import {
   signInWithEmailAndPassword,
   signInWithPopup,
   GoogleAuthProvider,
-  onAuthStateChanged,
-} from 'firebase/auth';
+} from "firebase/auth";
 import {
   addDoc,
   collection,
@@ -14,31 +13,26 @@ import {
   arrayUnion,
   arrayRemove,
   updateDoc,
-  serverTimestamp,
-  orderBy,
   getDoc,
-} from 'firebase/firestore';
-import { auth, db } from '../firebase';
+  //serverTimestamp,
+  //orderBy,
+} from "firebase/firestore";
+import { auth, db } from "../firebase";
 
-export const crearUsuarioConCorreoYContraseña = (email, contraseña) => createUserWithEmailAndPassword(auth, email, contraseña);
+export const crearUsuarioConCorreoYContraseña = (email, contraseña) =>
+  createUserWithEmailAndPassword(auth, email, contraseña);
 
-/* onAuthStateChanged(auth, (user) => {
-  if (user) {
-    onNavigate('/home');
-  } else {
-    onNavigate('/');
-  }
-}); */
-
-export const ingresarUsuarioConCorreoYContraseña = (email, contraseña) => signInWithEmailAndPassword(auth, email, contraseña);
+export const ingresarUsuarioConCorreoYContraseña = (email, contraseña) =>
+  signInWithEmailAndPassword(auth, email, contraseña);
 
 export const ingresarUsuarioConCuentaGoogle = () => {
   const provider = new GoogleAuthProvider();
   return signInWithPopup(auth, provider);
 };
 
+//Crea y guarda el post
 export const crearPost = async (text) => {
-  const createPost = await addDoc(collection(db, 'publicaciones'), {
+  const createPost = await addDoc(collection(db, "publicaciones"), {
     // date: serverTimestamp(), //para poner las fechas ordenasdas
     contenido: text,
     // usuario: user,
@@ -46,31 +40,30 @@ export const crearPost = async (text) => {
     userId: auth.currentUser.uid,
   });
 };
+//Muestra los post en pantalla
+export const mostrarPost = (callback) =>
+  onSnapshot(collection(db, "publicaciones"), callback);
 
-export const mostrarPost = (callback) => onSnapshot(collection(db, 'publicaciones'), callback);
+//Borrar post
+export const borrarPost = (id) => deleteDoc(doc(db, "publicaciones", id));
 
-/* export const mostrarPost = (callback) =>
-    onSnapshot(
-      collection(db, "publicaciones", orderBy(("date", "desc"))),
-      callback
-    ); */
-export const borrarPost = (id) => deleteDoc(doc(db, 'publicaciones', id));
-
-// 1:00:22 duda porque usa getTask //llegue al 1:02:58
-
-// export const getTask = (id) => getDoc(doc(db, "publicaciones", id));
-
-// Dar likes y contador de likes
+// Likes
 export const addLike = async (id, userLike) => {
-  await updateDoc(doc(db, 'publicaciones', id), {
+  await updateDoc(doc(db, "publicaciones", id), {
     likes: arrayUnion(userLike),
   });
 };
 export const removeLike = async (id, userLike) => {
-  await updateDoc(doc(db, 'publicaciones', id), {
+  await updateDoc(doc(db, "publicaciones", id), {
     likes: arrayRemove(userLike),
   });
 };
-
 export const getUser = () => auth.currentUser;
-export const getLikes = (id) => getDoc(doc(db, 'publicaciones', id));
+export const getLikes = (id) => getDoc(doc(db, "publicaciones", id));
+
+//Editar
+export const getPost = (id) => getDoc(doc(db, "publicaciones", id));
+
+export const updatePost = (postId, updatedData) => {
+  return updateDoc(doc(db, "publicaciones", postId), updatedData);
+};
