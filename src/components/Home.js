c0a9f465136089c34b04765b53c727a40164fd28
claudiaoj <claudiaortizjabre@gmail.com>
2023-06-27 22:30:32 -0400

    agrega likes y cambios en css

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,35 +10,25 @@ import {
   getPost,
   updatePost,
   getCurrentUser,
-} from '../lib/index.js';
+} from "../lib/index.js";
 
 export const Home = (onNavigate) => {
   // Variables de divs del Dom
-<<<<<<< HEAD
   const HomeDiv = document.createElement("div");
   const logoDiv = document.createElement("div");
   const logoImg = document.createElement("img");
   const postSection = document.createElement("section");
   const postDiv = document.createElement("form");
   const postFeedDiv = document.createElement("div");
-=======
-  const HomeDiv = document.createElement('div');
-  const logoDiv = document.createElement('div');
-  const logoImg = document.createElement('img');
-  const postSection = document.createElement('section');
-  const postDiv = document.createElement('div');
-  const postFeedDiv = document.createElement('div');
->>>>>>> 504bc37e30d13224c9672a4c47957e1802b2c4f8
   // Variable de botón salir
-  const buttonLogout = document.createElement('button');
-  const logoutImg = document.createElement('img');
+  const buttonLogout = document.createElement("button");
+  const logoutImg = document.createElement("img");
   // Variables de formulario
-  const titleWelcome = document.createElement('h2');
-  const postPublish = document.createElement('input');
-  const buttonPublish = document.createElement('button');
+  const titleWelcome = document.createElement("h2");
+  const postPublish = document.createElement("input");
+  const buttonPublish = document.createElement("button");
 
   // Atributos de variables DOM
-<<<<<<< HEAD
   HomeDiv.className = "container_all";
   HomeDiv.id = "id-container-all";
   logoDiv.className = "div_logo";
@@ -46,65 +36,57 @@ export const Home = (onNavigate) => {
   postSection.id = "id-section-post";
   postDiv.className = "container_post";
   postDiv.id = "id-container-post";
-=======
-  HomeDiv.className = 'container_all';
-  HomeDiv.id = 'id-container-all';
-  logoDiv.className = 'div_logo';
-  postSection.className = 'section_post';
-  postDiv.className = 'container_post';
-  postDiv.id = 'id-container-post';
->>>>>>> 504bc37e30d13224c9672a4c47957e1802b2c4f8
   // Atributo botón salir
-  logoutImg.className = 'img_logout';
-  logoutImg.src = 'images/logout1.png';
-  buttonLogout.className = 'logout_button';
+  logoutImg.className = "img_logout";
+  logoutImg.src = "images/logout1.png";
+  buttonLogout.className = "logout_button";
 
   // Atributos de imagen del título
-  logoImg.className = 'img_logo';
-  logoImg.src = 'images/MonuTrip1.png';
+  logoImg.className = "img_logo";
+  logoImg.src = "images/MonuTrip1.png";
   // Atributos de formulario
-  titleWelcome.className = 'welcome_title';
-  titleWelcome.textContent = 'Bienvenida(o)';
-  postPublish.id = 'post-input';
-  postPublish.className = 'post_input';
-  postPublish.placeholder = ' Comparte tu experiencia....';
+  titleWelcome.className = "welcome_title";
+  titleWelcome.textContent = "Bienvenida(o)";
+  postPublish.id = "post-input";
+  postPublish.className = "post_input";
+  postPublish.placeholder = " Comparte tu experiencia....";
   // postPublish.rows = "4"; // para que sean 4 lineas
-  buttonPublish.className = 'publish_button';
-  buttonPublish.textContent = 'Publicar';
+  buttonPublish.className = "publish_button";
+  buttonPublish.textContent = "Publicar";
   // Atributos de publicaciones creadas
-  postFeedDiv.id = 'post-feed';
+  postFeedDiv.id = "post-feed";
 
   // Event Listener de botones
 
   // Botón salir
-  buttonLogout.addEventListener('click', () => onNavigate('/'));
+  buttonLogout.addEventListener("click", () => onNavigate("/"));
 
   // Botón publicar
-  buttonPublish.addEventListener('click', async (e) => {
+  buttonPublish.addEventListener("click", async (e) => {
     e.preventDefault();
     if (editMode === false) {
-      const contentInput = document.getElementById('post-input').value;
-      crearPost(contentInput).then(() => { });
+      const contentInput = document.getElementById("post-input").value;
+      crearPost(contentInput).then(() => {});
     } else {
-      const postInput = document.querySelector('#post-input');
+      const postInput = document.querySelector("#post-input");
       const updatedContent = postInput.value;
       await updatePost(postIdEditing, { contenido: updatedContent });
       mostrarPost();
       editMode = false;
-      postIdEditing = '';
+      postIdEditing = "";
     }
   });
-  let postIdEditing = '';
+  let postIdEditing = "";
   let editMode = false;
 
   // Muestra los post, botonos editar, eliminar y likes en pantalla
   mostrarPost((querySnapshot) => {
-    let html = '';
+    let html = "";
     querySnapshot.forEach((doc) => {
       const currentUser = getCurrentUser();
       const dataLikes = doc.data();
       const userLike = dataLikes.likes.includes(currentUser.uid);
-      const imgLikeButton = userLike ? 'like.png' : 'dislike.png';
+      const imgLikeButton = userLike ? "like.png" : "dislike.png";
       const publicacion = doc.data();
       html += `
       <div class="container_feed_post" data-id="${doc.id}">
@@ -130,23 +112,23 @@ export const Home = (onNavigate) => {
 
     // Limpia input
     function limpiarInput() {
-      document.getElementById('post-input').value = '';
+      document.getElementById("post-input").value = "";
     }
 
     // Boton borrar
-    const buttonDelete = postFeedDiv.querySelectorAll('.button_delete');
+    const buttonDelete = postFeedDiv.querySelectorAll(".button_delete");
     buttonDelete.forEach((btn) => {
-      btn.addEventListener('click', ({ target: { dataset } }) => {
+      btn.addEventListener("click", ({ target: { dataset } }) => {
         borrarPost(dataset.id);
       });
     });
 
     // Editar post
-    const postForm = document.getElementById('id-container-post');
-    const buttonEdit = postFeedDiv.querySelectorAll('.button_edit');
+    const postForm = document.getElementById("id-container-post");
+    const buttonEdit = postFeedDiv.querySelectorAll(".button_edit");
 
     buttonEdit.forEach((btn) => {
-      btn.addEventListener('click', async (e) => {
+      btn.addEventListener("click", async (e) => {
         // console.log(editMode);
         editMode = true;
         // console.log(editMode);
@@ -154,7 +136,7 @@ export const Home = (onNavigate) => {
         postIdEditing = postId;
         const doc = await getPost(postId);
         const task = doc.data(); // accede a los datos del post
-        const postInput = postForm.querySelector('#post-input');
+        const postInput = postForm.querySelector("#post-input");
         if (postInput) {
           postInput.value = task.contenido;
         }
@@ -164,9 +146,9 @@ export const Home = (onNavigate) => {
 
   // Boton de likes
   function likeEvent() {
-    const likeButton = postFeedDiv.querySelectorAll('.like_btn'); // tomamos el valor del selector
+    const likeButton = postFeedDiv.querySelectorAll(".like_btn"); // tomamos el valor del selector
     likeButton.forEach((button) => {
-      button.addEventListener('click', () => {
+      button.addEventListener("click", () => {
         const likeValue = button.id;
         const userId = getUser().uid;
         getLikes(likeValue).then((postLike) => {
